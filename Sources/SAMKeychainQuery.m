30ca878d38a36808072f1a4553c2f55dcc581b06
Tim Johnsen <timonus@gmail.com>
2020-02-26 20:36:29 -0800

    Use secure NSKeyedArchiver methods.

--- Sources/SAMKeychainQuery.m
+++ Sources/SAMKeychainQuery.m
@@ -158,13 +158,13 @@
 #pragma mark - Accessors
 
 - (void)setPasswordObject:(id<NSCoding>)object {
-	self.passwordData = [NSKeyedArchiver archivedDataWithRootObject:object];
+	self.passwordData = [NSKeyedArchiver archivedDataWithRootObject:object requiringSecureCoding:NO error:nil];
 }
 
 
 - (id<NSCoding>)passwordObject {
 	if ([self.passwordData length]) {
-		return [NSKeyedUnarchiver unarchiveObjectWithData:self.passwordData];
+		return [NSKeyedUnarchiver unarchivedObjectOfClass:[NSObject class] fromData:self.passwordData error:nil];
 	}
 	return nil;
 }
